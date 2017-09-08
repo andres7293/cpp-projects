7e375b16da5a2ab91bb01b8ce0c96b265ac9d6df
andres7293 <andres7293@gmail.com>
2017-09-08 12:10:22 +0100

    refactor classes

--- classes/rectangle.cpp
+++ classes/rectangle.cpp
@@ -2,25 +2,28 @@
 #include "rectangle.h"
 using namespace std;
 
-void Rectangle::set_high(int high){
-  High=high;
+void Rectangle::set_high(int high)
+{
+ this->High=high;
 }
 
-void Rectangle::set_width(int width){
-  Width=width;
+void Rectangle::set_width(int width)
+{
+  this->Width=width;
 }
 
-void Rectangle::print_rectangle(void){
+void Rectangle::print_rectangle(void)
+{
   cout << "Size of rectangle :\n\r";
-  cout <<  "High = " << High << " ";
-  cout <<  "Width = " << Width << "\n\r";
+  cout <<  "High = " << this->High << " ";
+  cout <<  "Width = " << this->Width << "\n\r";
 }
 
 //Constructor
 Rectangle::Rectangle (int high,int width)
 {
-  High = high;
-  Width = width;
+  this->High = high;
+  this->Width = width;
 }
 
 //empty constructor
